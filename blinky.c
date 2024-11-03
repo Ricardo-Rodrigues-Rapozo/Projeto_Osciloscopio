d3ff05dddd4f3f34cb85bd1f111ea4e5d0a284e0
Ricardo Rodrigues Rapozo <92274024+Ricardo-Rodrigues-Rapozo@users.noreply.github.com>
2024-11-03 19:10:05 -0300

    Update blinky.c
    Adição do PWM para validar os resultados do osciloscópio

--- blinky.c
+++ blinky.c
@@ -11,23 +11,67 @@
 #include "driverlib/timer.h"
 #include "inc/hw_ints.h"
 #include "driverlib/interrupt.h"
+#include "driverlib/debug.h"
+#include "driverlib/pwm.h"
+#include "driverlib/fpu.h"
+
 
 //
 //--------------------------- PARAMETROS ---------------------------------------------------------------------
 //
 
-#define SIZE_BUFFER 1000
-int buffer[SIZE_BUFFER];
+#define PWM_FREQUENCY 100
+#define APP_PI 3.1415926535897932384626433832795f
+#define STEPS 256
+uint32_t ui32StoredPWM;
+#define SIZE_BUFFER 5000
+uint16_t  buffer[SIZE_BUFFER];
 volatile uint8_t flag = 0;
-volatile uint32_t ui32ADCAvg; // Média das leituras do ADC
 uint16_t cont = 0;
-uint32_t FS = 5000; // Frequência de amostragem
+uint32_t FS = 40000; // Frequência de amostragem
 uint8_t SYNC_BYTE = 1;  // Sinal de sincronização esperado
 uint32_t ui32ADC0Value[1]; // Armazena valores do ADC
+uint16_t adcValue16;
 uint32_t control1 = 0; // variavel para debug
 uint32_t control2 = 0; // variavel para debug
 uint32_t ui32SysClkFreq;
-uint16_t i;
+uint32_t timerLoad;
+volatile uint32_t ui32Load; // PWM period
+volatile uint32_t ui32BlueLevel; // PWM duty cycle for blue LED
+volatile uint32_t ui32PWMClock; // PWM clock frequency
+volatile uint32_t ui32Index; // Counts the calculation loops
+float fAngle; // Value for sine math (radians)
+
+
+//
+// ---------------------Configuração PWM ----------------------------------------------------------------------
+//
+void InitPWM(void)
+{
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOG); // Change these pins later
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOF); // Change these pins later
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_PWM0); // Check where this PWM has it's generation on
+
+    GPIOPinTypeGPIOOutput(GPIO_PORTF_BASE, GPIO_PIN_2|GPIO_PIN_3); // Change these pins later
+    GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_2|GPIO_PIN_3, 0x00); // Change these pins later
+
+    PWMClockSet(PWM0_BASE,PWM_SYSCLK_DIV_8 ); //Setting frenquency
+
+    GPIOPinConfigure(GPIO_PG0_M0PWM4);
+    GPIOPinTypePWM(GPIO_PORTG_BASE, GPIO_PIN_0);
+
+    ui32PWMClock = ui32SysClkFreq / 64; // 120MHz/64
+    ui32Load = (ui32PWMClock / PWM_FREQUENCY) - 1; // 1875000/100
+
+    PWMGenConfigure(PWM0_BASE, PWM_GEN_2, PWM_GEN_MODE_DOWN);
+    PWMGenPeriodSet(PWM0_BASE, PWM_GEN_2, ui32Load);
+    PWMPulseWidthSet(PWM0_BASE, PWM_OUT_4, ui32Load/2);
+    PWMOutputState(PWM0_BASE, PWM_OUT_4_BIT, true);
+    PWMGenEnable(PWM0_BASE, PWM_GEN_2);
+
+    ui32Index = 0;
+
+}
 
 //
 //--------------------Interrupção para RX UART ----------------------------------------------------------------
@@ -85,10 +129,10 @@ void UARTInit(void) {
 //
 //------------------------- Função para enviar dados via UART -------------------------------------------
 //
-void sendINT32(int* buffer) {// Função para enviar 4 bytes via UART
+void sendINT32(uint16_t* buffer) {// Função para enviar 4 bytes via UART
     uint_fast16_t i;
     char* ptr = (char*)buffer;
-    for ( i = 0; i < SIZE_BUFFER*sizeof(int) ; i++)
+    for ( i = 0; i < SIZE_BUFFER*sizeof(uint16_t) ; i++)
     {
         UARTCharPut(UART0_BASE, ptr[i]);  // Envia cada byte
     }
@@ -124,7 +168,7 @@ void InitADC(void)
 //
 
            TimerConfigure(TIMER0_BASE, TIMER_CFG_PERIODIC);
-           uint32_t timerLoad = (SysCtlClockGet() / FS) - 1; // Configura para uma taxa de FS kHz
+           timerLoad = (SysCtlClockGet() / FS) - 1; // Configura para uma taxa de FS kHz
            TimerLoadSet(TIMER0_BASE, TIMER_A, timerLoad);
            TimerControlTrigger(TIMER0_BASE, TIMER_A, true); // Permite que o Timer dispare o ADC
            TimerEnable(TIMER0_BASE, TIMER_A);
@@ -150,12 +194,12 @@ void InitADC(void)
 
 int main(void)
 {
+    int idx=0;
 //
 // -------------------------- Configurações de clock e periféricos -------------------------------------------------------
 // Configura o clock do sistema para 120 MHz
 
     ui32SysClkFreq = SysCtlClockFreqSet((SYSCTL_XTAL_25MHZ | SYSCTL_OSC_MAIN | SYSCTL_USE_PLL | SYSCTL_CFG_VCO_480), 120000000);
-
 //
 // -------------------------- Inicialização da UART  ----------------------------------------------------------------------
 //
@@ -168,30 +212,35 @@ int main(void)
     InitADC();
 
 //
-// -------------------------- BUFFER ----------------------------------------------------------------------
+// -------------------------- Inicialização do PWM ----------------------------------------------------------------------
 //
 
+    InitPWM();
+    ui32StoredPWM = ui32Load/2; // 1% de ciclo de trabalho
+    PWMPulseWidthSet(PWM0_BASE, PWM_OUT_4, ui32StoredPWM);
 // -------------------------- Loop Principal ------------------------------------------------------------------
 //
 
     while(1)
     {
+
+        //SysCtlDelay(ui32SysClkFreq/(STEPS));
         // Espera até a conversão do ADC estar completa
         if (ADCIntStatus(ADC0_BASE, 1, false))
         {
-            // Limpa a flag de interrupção do ADC e obtém os dados do sequenciador
+            //Limpa a flag de interrupção do ADC e obtém os dados do sequenciador
             ADCIntClear(ADC0_BASE, 1);
             ADCSequenceDataGet(ADC0_BASE, 1, ui32ADC0Value);
             control2 = 2;
-            for(i =0; i < SIZE_BUFFER; i++)
-            {
-                buffer[i] = ui32ADC0Value[0];
-            }
+            buffer[idx] = (uint16_t) (ui32ADC0Value[0] & 0xFFFF);  // Mantém apenas os 16 bits menos significativos
+            idx = (idx+1)%SIZE_BUFFER;
+
             if (flag == 1)
             {
+                control2 = 10;
                 sendINT32(buffer);
+                flag = 0;
             }
-
         }
     }
 }
